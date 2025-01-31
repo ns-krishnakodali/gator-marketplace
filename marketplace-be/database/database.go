2e73b677a5fcdd0923909af25debdc2292629e42
Krishna Kodali <85895093+KrishnaKodali99@users.noreply.github.com>
2025-01-31 15:06:30 -0500

    feat: Integrate PostgreSQL with the server framework (BE)

---
+++ marketplace-be/database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+var err error
+
+func InitDatabase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	log.Println("Connected to the database successfully!")
+}
