e9535851697ac1928988870395f10f985a71289b
Krishna Kodali <85895093+KrishnaKodali99@users.noreply.github.com>
2025-02-01 18:19:50 -0500

    feat: Configure jwt middleware and authentatication
    * chore: Modify the database setup with GORM
    
    * feat: Configure jwt middleware and authentatication

---
+++ marketplace-be/auth/jwt.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func GenerateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return token.SignedString(jwtSecret)
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		tokenString := context.GetHeader("Authorization")
+		if tokenString == "" {
+			context.JSON(http.StatusUnauthorized, gin.H{"error": "No token provided"})
+			context.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			context.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			context.Abort()
+			return
+		}
+
+		context.Next()
+	}
+}
