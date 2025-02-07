33364a6294c9870e2e8e702d5a6eea495e812b03
Krishna Kodali <krishnakodali81@gmail.com>
2025-02-07 17:18:57 -0500

    fix: Add auth form and reformat login/signup components

--- marketplace-fe/src/app/features/auth/components/login/login.component.ts
+++ marketplace-fe/src/app/features/auth/components/login/login.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 
-import { ButtonComponent, InputComponent, TextComponent } from '../../shared-ui'
+import { ButtonComponent, InputComponent, TextComponent } from '../../../../shared-ui'
+import { FormComponent } from '../form/form.component'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [MatButtonModule, ButtonComponent, InputComponent, TextComponent],
+  imports: [MatButtonModule, ButtonComponent, InputComponent, TextComponent, FormComponent],
 })
 export class LoginComponent {
   email?: string
   password?: string
 
-  onClick = (): void => {
+  onSubmit = (): void => {
     if (this.email === 'user@example.com' && this.password === 'password123') {
       alert('Login successful!')
     }
--- marketplace-fe/src/app/features/auth/components/signup/signup.component.ts
+++ marketplace-fe/src/app/features/auth/components/signup/signup.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 
-import { ButtonComponent, InputComponent, TextComponent } from '../../shared-ui/'
+import { FormComponent } from '../form/form.component'
+
+import { ButtonComponent, InputComponent, TextComponent } from '../../../../shared-ui'
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
-  imports: [MatButtonModule, ButtonComponent, InputComponent, TextComponent],
+  imports: [MatButtonModule, ButtonComponent, InputComponent, TextComponent, FormComponent],
 })
 export class SignupComponent {
   username?: string
