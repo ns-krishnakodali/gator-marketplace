2c724e6d3d8729cc6d5a809babcb5f64900f5d62
Krishna Kodali <krishnakodali81@gmail.com>
2025-02-10 01:37:38 -0500

    fix: Update default behavior of auth forms and add Sprint 1 retrospective notes

--- marketplace-fe/src/app/features/auth/components/form/form.component.ts
+++ marketplace-fe/src/app/features/auth/components/form/form.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+
 import { TextComponent } from '../../../../shared-ui/'
 
 @Component({
   selector: 'app-form',
-  imports: [TextComponent],
+  imports: [TextComponent, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
 export class FormComponent {
   @Input({ required: true }) id!: string
   @Input({ required: true }) heading!: string
-  @Input({ required: true }) onSubmit!: () => void
+  
+  @Output() submitForm = new EventEmitter<Event>();
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this.submitForm.emit(event);
+  }
 }
