6e428574485f975b48b354dffdad9ef9784cb078
Harshak Parmar <harshakparmar12@gmail.com>
2025-02-27 19:13:05 -0500

    feat: added hamburger icon, removed RouterModule,  and added onClick

--- marketplace-fe/src/app/shared-ui/sidebar/sidebar.component.ts
+++ marketplace-fe/src/app/shared-ui/sidebar/sidebar.component.ts
@@ -1,16 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  @Input() menuItems: { label: string; route: string }[] = [];
+
+  @Input() menuItems: { label: string; onClick: () => void }[] = [];
   @Input() isExpanded: boolean = true;
 
   toggleSidebar() {
