1d276b22489edca266fde62fc28eda4832c5aab3
Harshak Parmar <66898184+hpamdeoxys@users.noreply.github.com>
2025-02-27 20:19:20 -0500

    feat: Implement sidebar
    * feat: updated sidebar
    
    * feat: updated sidebar.component.css
    
    * feat: added hamburger icon, removed RouterModule,  and added onClick
    
    * feat: cleaned app.component.css
    
    * feat: fixed the structure of sidebar component

--- marketplace-fe/src/app/app.component.ts
+++ marketplace-fe/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core'
-import { RouterModule } from '@angular/router'
-
-import { NotificationsComponent } from './shared-ui/'
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NotificationsComponent } from './shared-ui/';
+import { SidebarComponent } from './features/products/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, NotificationsComponent],
+  imports: [RouterModule, NotificationsComponent, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'], 
 })
-export class AppComponent {}
+export class AppComponent {
+  menuItems = [
+    { label: 'Home', onClick: () => { console.log('Home clicked'); }, route: '/home' }, 
+    { label: 'Profile', onClick: () => { console.log('Profile clicked'); }, route: '/profile' }  
+  ];
+  isExpanded = true;  
+}
