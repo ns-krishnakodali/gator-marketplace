1e2fa0aade01507ec153a07cf58a4c4dd93c2646
Anshita Rao <royalanshita@gmail.com>
2025-04-21 21:22:55 -0400

    feat: list product form test

--- marketplace-fe/src/app/features/list-product/components/list-product-form/list-product-form.component.spec.ts
+++ marketplace-fe/src/app/features/list-product/components/list-product-form/list-product-form.component.spec.ts
@@ -1,22 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Categories } from '../../../products/models'
+
 
 import { ListProductFormComponent } from './list-product-form.component'
 
+
 describe('ListProductFormComponent', () => {
-  let component: ListProductFormComponent
-  let fixture: ComponentFixture<ListProductFormComponent>
+ let component: ListProductFormComponent
+ let fixture: ComponentFixture<ListProductFormComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListProductFormComponent],
-    }).compileComponents()
 
-    fixture = TestBed.createComponent(ListProductFormComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
-  })
+ beforeEach(async () => {
+   await TestBed.configureTestingModule({
+     imports: [ListProductFormComponent],
+   }).compileComponents()
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+
+   fixture = TestBed.createComponent(ListProductFormComponent)
+   component = fixture.componentInstance
+   fixture.detectChanges()
+ })
+
+
+ it('should create', () => {
+   expect(component).toBeTruthy()
+ })
+
+
+ it('should return category values from Categories enum', () => {
+   const fixture = TestBed.createComponent(ListProductFormComponent)
+   const component = fixture.componentInstance
+   const expected = Object.values(Categories)
+   expect(component.categoryValues).toEqual(expected)
+ })
 })
+
+
+
+
